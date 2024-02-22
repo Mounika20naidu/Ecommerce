c77c67bc0955bf90d5be155ddb01ce90bdb5f1b2
Mounika20naidu <160842281+Mounika20naidu@users.noreply.github.com>
2024-02-22 17:41:37 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footerSection'>
+       <div className="bannerBox">
+     <h2>LIFESTYLE</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
